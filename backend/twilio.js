396c68e1026e144a135786603b333f5ed9168fb6
pstreccius-hue <pstreccius@twilio.com>
2026-02-05 10:17:19 -0700

    Add Twilio integration for SMS and voice calls

---
+++ backend/twilio.js
@@ -0,0 +1,14 @@
+const twilio = require('twilio');
+const { TWILIO_SID, TWILIO_TOKEN, TWILIO_NUMBER } = process.env;
+
+const client = twilio(TWILIO_SID, TWILIO_TOKEN);
+
+exports.sendSms = (to, body) =>
+  client.messages.create({ body, to, from: TWILIO_NUMBER });
+
+exports.sendVoice = (to, name, program) =>
+  client.calls.create({
+    to,
+    from: TWILIO_NUMBER,
+    twiml: `<Response><Say voice="alice">Hello ${name}, this is Carelon Health! Congratulations on starting the ${program} program. If you have questions, reply YES to your welcome text.</Say></Response>`
+  });
