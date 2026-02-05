c915a1eb9235039c987c163d8eb125f95a984284
pstreccius-hue <pstreccius@twilio.com>
2026-02-05 11:37:16 -0700

    Update segment.js

--- backend/segment.js
+++ backend/segment.js
@@ -3,12 +3,12 @@ const SEGMENT_WRITE_KEY = process.env.SEGMENT_WRITE_KEY;
 
 exports.sendIdentify = user => axios.post(
   'https://api.segment.io/v1/identify',
-  { userId: user.email, traits: { name: user.name, email: user.email, phone: user.phone, program: user.program } },
+  { userId: user.email || user.phone, traits: { name: user.name, email: user.email, phone: user.phone, program: user.program } },
   { auth: { username: SEGMENT_WRITE_KEY, password: '' } }
 );
 
 exports.sendTrack = (user, event, properties) => axios.post(
   'https://api.segment.io/v1/track',
-  { userId: user.email, event, properties },
+  { userId: user.email || user.phone, event, properties },
   { auth: { username: SEGMENT_WRITE_KEY, password: '' } }
 );
